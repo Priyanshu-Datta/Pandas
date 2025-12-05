61473ebd05c372bcdabede68d71f2c73da34a452
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-12-06 01:17:35 +0530

    update the existing data

---
+++ updating.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+data ={
+    "Name":["Ram","Shyam","jadu","madhu","Rahul","Ranit","Priyanshu","sanchari"],
+    "Age": [28,31,32,56,45,34,21,20],
+    "Salary":[30000,40000,50000,34000,45000,53000,70000,60000],
+    "performance Score":[85,87,91,78,80,88,95,96]
+}
+
+df = pd.DataFrame(data)
+print("Sample Dtaframe")
+print(df)
+
+# update salary of one person 
+df.loc[6,'Salary'] = 75000
+print(df)
