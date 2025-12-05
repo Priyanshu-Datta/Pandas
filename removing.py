997deb89f8863a260b1128ccef7868fc14aacff8
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-12-06 01:16:46 +0530

    removing cilumns

---
+++ removing.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+
+data ={
+    "Name":["Ram","Shyam","jadu","madhu","Rahul","Ranit","Priyanshu","sanchari"],
+    "Age": [28,31,32,56,45,34,21,20],
+    "Salary":[30000,40000,50000,34000,45000,53000,70000,60000],
+    "performance Score":[85,87,91,78,80,88,95,96]
+}
+
+df = pd.DataFrame(data)
+print("Sample Dtaframe")
+print(df)
+
+# remove single column 
+print("modified table")
+df.drop(columns=["performance Score"],inplace=True)
+print(df)
+
+# remove multiple column 
+df.drop(columns=["Salary","Age"],inplace=True)
+print(df)
